5c1da3a446ce26831ae5d3e4192780d4bedeab51
vladdicckk <92015183+vladdicckk@users.noreply.github.com>
2022-02-08 18:45:43 +0200

    Add files via upload

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,7 +1,22 @@
 //package domain;
 
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    public static void main(String[] args) {
+        int tmp = getSumDigitsOfData("20,02,06", 0);
+        if (tmp/10 != 0) {
+            System.out.println(tmp + ";");
+            System.out.println(getSumDigitsOfData(String.valueOf(tmp), 0) + ";");
+        } else System.out.println(tmp + ";");
+    }
+ 
+    private static int getSumDigitsOfData(String data, int indexChar) {
+        if(indexChar > data.length() - 1) return 0;
+        return data.charAt(indexChar) <= '9' && data.charAt(indexChar) > '0' ?
+                (data.charAt(indexChar) - '0') + getSumDigitsOfData(data, indexChar + 1) :
+                getSumDigitsOfData(data, indexChar + 1);
+    }
+
+    static String Calculate(int R) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
